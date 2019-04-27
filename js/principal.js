fa0628e33b901ccd341296074f6639d1d4e6bbad
AnotherAlexMoreira <alexmartinhomoreira@gmail.com>
2019-04-27 10:59:32 -0300

    Aula 4 - Eventos, Formulários e Criando Elementos

--- js/principal.js
+++ js/principal.js
@@ -38,5 +38,49 @@ for(var i = 0; i < pacientes.length; i ++){
     }
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+//adiciona um escutador de evento
+botaoAdicionar.addEventListener("click", function(event){
+    //faz com que o botão perca seu comportamento padrão de enviar e apagar o form
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    //criando a tr dentro da tabela
+    var pacienteTr = document.createElement("tr");
+
+    //criando tds dentro da tr na tabela
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    //passando os valores para dentro das tds
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    //colocando as tds dentro da tag tr
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    
+    //colocando a tr dentro da tabela
+    tabela.appendChild(pacienteTr);
+
+    
+})
+
+
 
     
