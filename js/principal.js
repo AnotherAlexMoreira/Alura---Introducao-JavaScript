2f8a04710d06f92a452ec0865291c4a59c57e6bd
AnotherAlexMoreira <alexmartinhomoreira@gmail.com>
2019-04-27 09:42:05 -0300

    Aula 3 - Arrays, Loops e Estilos

--- js/principal.js
+++ js/principal.js
@@ -1,28 +1,42 @@
-var paciente = document.querySelector("#primeiro-paciente");
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+//querySelector só mostra o primeiro registro
+//var paciente = document.querySelector("#primeiro-paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+//querySelectorAll traz uma lista de todos os registros
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdImc = paciente.querySelector(".info-imc");
+for(var i = 0; i < pacientes.length; i ++){
 
-var pesoValido = true;
-var alturaValida = true;
+    var paciente = pacientes[i];
 
-if (peso <= 0 || peso >= 1000) {
-    pesoValido = false;
-    tdImc.textContent = "Peso inválido";
-}
-if (altura <= 0 || altura >= 3.00){
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    if (altura <= 0 || altura >= 3.00){
         alturaValida = false;
         tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+    if (pesoValido && alturaValida){
+        var imc = peso / (altura*altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
-if (pesoValido && alturaValida){
-    var imc = peso / (altura*altura);
-    tdImc.textContent = imc;
-}else{
-    tdImc.textContent = "Altura e/ou peso inválido(s)!"
-}
+
+
     
